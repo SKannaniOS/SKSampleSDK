2569d8950f40a7c0dbcd5edc9f5544edee9b8fe8
Satheesh Kannan <satheeshkannan.ios@gmail.com>
2022-11-15 22:43:21 +0530

    .gitignore is now working

--- SKSampleSDK/SKSampleClass.swift
+++ SKSampleSDK/SKSampleClass.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+public final class BasicMathClass {
+    
+    static let simpleObject = BasicMathClass()
+    
+    private init() {}
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+    public func mul(a: Int, b: Int) -> Int {
+        return a * b
+    }
+    
+    public func div(a: Int, b: Int) -> Double {
+        return Double(a / b)
+    }
+    
+}
